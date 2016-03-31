c87618fb74f764dbfa2e689c3610ccbf7cbe4b3e
FalconDesign <hebgax0@gmail.com>
2016-03-31 04:42:42 +0300

    1. Created Header
    Do not forger to give nav a hover!

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,1 +1,7 @@
+ document.addEventListener("DOMContentLoaded", function() {
 
+  document.querySelector('.header-1').classList.add('animation');
+  document.querySelector('.header-2').classList.add('animation');
+  document.querySelector('.header-3').classList.add('animation');
+
+ });
