98cf345b1cd62ef824949292e4ea5bbeaf62f7cc
FalconDesign <hebgax0@gmail.com>
2016-04-01 02:46:15 +0300

    2. Resposive header!
    1.Responsive header
    2.We have some interactive stuffs)
    3.I am happy)

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,7 +1,38 @@
- document.addEventListener("DOMContentLoaded", function() {
 
-  document.querySelector('.header-1').classList.add('animation');
-  document.querySelector('.header-2').classList.add('animation');
-  document.querySelector('.header-3').classList.add('animation');
+window.addEventListener("load", function() {
 
- });
+   document.querySelector('.header-1').classList.add('animation');
+   document.querySelector('.header-2').classList.add('animation');
+   document.querySelector('.header-3').classList.add('animation');
+
+  });
+
+var burger = document.querySelector('.burger');
+
+burger.addEventListener('click', hoverBurger);
+
+function hoverBurger() {
+  if (burger.classList.contains('is-open')) {
+    burger.classList.remove('is-open');
+
+    var kicker = document.querySelector('.kicker');
+    kicker.style = 'height: 0px';
+
+    var allAnckers = document.querySelectorAll('.kicker nav a');
+
+    for (var i = 0; i < allAnckers.length; i++) {
+      allAnckers[i].style = 'color: transparent;';
+    }
+
+  } else if (!burger.classList.contains('is-open')) {
+    burger.classList.add('is-open');
+
+    var kicker = document.querySelector('.kicker');
+    kicker.style = 'height: 90px';
+
+    var allAnckers = document.querySelectorAll('.kicker nav a');
+    for (var i = 0; i < allAnckers.length; i++) {
+      allAnckers[i].style = 'color: white;';
+    }
+  }
+}
