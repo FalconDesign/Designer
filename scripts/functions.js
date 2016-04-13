bd77fe7d76b29588fbd672237ece7beacc64d0f4
FalconDesign <hebgax0@gmail.com>
2016-04-13 22:00:24 +0300

    SPINNER!!!!!!!!!!!

--- scripts/functions.js
+++ scripts/functions.js
@@ -11,11 +11,11 @@
 
 $(window).load( function () {
 
-    $('.spinner').hide();
+
    document.querySelector('.header-1').classList.add('animation');
    document.querySelector('.header-2').classList.add('animation');
    document.querySelector('.header-3').classList.add('animation');
-
+    $('.spinner').hide();
 
 
 })
